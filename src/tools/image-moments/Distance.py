1827a71de36a000d6ec035ae5fe921a9978c2fb1
Mahsa Kalam <klammhsa@gmail.com>
2022-09-12 19:56:35 +0430

    adding image moments

---
+++ src/tools/image-moments/Distance.py
@@ -0,0 +1,34 @@
+import cv2
+
+im1 = cv2.imread('PET.jpg',cv2.IMREAD_GRAYSCALE)
+
+def rescaleFrame(im1, scale = 0.75):
+    # images, videos and live videos
+    width = int(im1.shape[1] * scale)
+    height = int(im1.shape[1] * scale)
+    dimensions = (width, height)
+
+    return cv2.resize(im1, dimensions, interpolation=cv2.INTER_AREA)
+
+resized_image = rescaleFrame(im1)
+
+im2 = cv2.imread('PET-6.jpg',cv2.IMREAD_GRAYSCALE)
+
+def rescaleFrame(im2, scale = 0.75):
+    # images, videos and live videos
+    width = int(im2.shape[1] * scale)
+    height = int(im2.shape[1] * scale)
+    dimensions = (width, height)
+
+    return cv2.resize(im2, dimensions, interpolation=cv2.INTER_AREA)
+
+resized_image = rescaleFrame(im2)
+
+d1 = cv2.matchShapes(im1,im2,cv2.CONTOURS_MATCH_I1,0)
+print(d1)
+
+d2 = cv2.matchShapes(im1,im2,cv2.CONTOURS_MATCH_I2,0)
+print(d2)
+
+d3 = cv2.matchShapes(im1,im2,cv2.CONTOURS_MATCH_I3,0)
+print(d3)
